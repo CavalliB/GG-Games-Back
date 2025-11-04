f215cf096bab45d7096727dbd0938d49fb90e6d8
CavalliB <119020580+CavalliB@users.noreply.github.com>
2025-11-04 22:15:30 +0000

    Conexion con Supabase

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import { supabase } from "./Supabase.js";
 dotenv.config();
 
 const app = express();
@@ -10,10 +10,23 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/Usuario", async (req, res) => {
+  try {
+    const { data: Usuario, error } = await supabase
+      .from("Usuario")
+      .select("*");
+
+    if (error) throw error;
+    res.status(200).json(Usuario);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.get("/", (req, res) => {
-    res.send("Servidor funcionando correctamente 🚀");
+  res.send("Servidor funcionando correctamente ");
 });
 
 app.listen(PORT, () => {
-    console.log(`✅ Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
